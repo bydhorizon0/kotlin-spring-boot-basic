34ebc610b6640bf4b6758d95df5eaf9c4792365d
bydhorizon0 <bydhorizon0@gmail.com>
2025-04-09 00:37:21 +0900

    @Validated 애노테이션 제거, signup 메서드에 Form 클래스 추가 (thymeleaf)

--- src/main/kotlin/com/example/kopring/controller/AuthController.kt
+++ src/main/kotlin/com/example/kopring/controller/AuthController.kt
@@ -6,19 +6,18 @@ import jakarta.validation.Valid
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
-@Validated
 @RequestMapping("/auth")
 @Controller
 class AuthController(private val authService: AuthService) {
 
     @GetMapping("/signup")
-    fun singup(): String {
+    fun signup(model: Model): String {
+        model.addAttribute("signupForm", SignupForm())
         return "auth/signup"
     }
 
