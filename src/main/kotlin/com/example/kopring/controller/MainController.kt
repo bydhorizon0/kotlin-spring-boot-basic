69e3bcfcebf3fd3d2ff21d73791a00e295298ca4
bydhorizon0 <bydhorizon0@gmail.com>
2025-04-12 02:02:17 +0900

    MainController 추가

--- src/main/kotlin/com/example/kopring/controller/MainController.kt
+++ src/main/kotlin/com/example/kopring/controller/MainController.kt
@@ -1,4 +1,11 @@
 package com.example.kopring.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
 class MainController {
+
+    @GetMapping
+    fun index(): String = "/main/main"
 }
