c0632909fcea5ca09228d9d42ab2bb6407b51136
bydhorizon0 <bydhorizon0@gmail.com>
2025-04-12 02:03:00 +0900

    User.kt 클래스 LocalDateTime 기본값 생성 - save 메서드로 저장할 때 기본값이 있어야한다.

--- src/main/kotlin/com/example/kopring/domain/user/User.kt
+++ src/main/kotlin/com/example/kopring/domain/user/User.kt
@@ -9,6 +9,6 @@ data class User(
     @Id
     val email: String,
     val password: String,
-    val createdAt: LocalDateTime? = null,
-    val updatedAt: LocalDateTime? = null,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime = LocalDateTime.now(),
 )
